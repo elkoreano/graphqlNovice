6e2f46aea0e1ed57cb8a74f6b1b6edee2cab4045
elkoreano <criistiian-95@hotmail.com>
2023-09-16 23:13:47 -0500

    App jsx changed

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -5,8 +5,55 @@ import { useState } from 'react'
 // import './App.css'
 
 // New modules
-import { ApolloClient, inMemoryCache, ApolloProvider, useLazyQuery, gql } from '@apollo/client'
+import { ApolloClient, inMemoryCache, ApolloProvider, useLazyQuery, gql } from '@apollo/client';
+import { Container, Row, Form, Button } from "react-bootstrap"
 
+// Random stuff imported by itself xd
+import e from 'express';
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new inMemoryCache(),
+})
+
+const HELLO_QUERY = gql`
+query Hello($message: String!) {
+  hello(message: $message)
+}
+`;
+
+// This function was created
+function hello() {
+  const [message, setMessage] = useState("");
+  const [getGreating, {loading, error, data}] = useLazyQuery(HELLO_QUERY);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getGreating({variables: {message}})
+  }
+
+  if(loading) return <p>Cargando</p>;
+  if(error) return <p>Error :v</p>;
+
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+      <Form.Group controlID= "formMessage">
+      <Form.Control type= "text" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Escribe un mensaje"
+      />
+      </Form.Group>
+      <Button className="nt-2" variant="primary" type="submit">
+        Enviar
+      </Button>
+      </Form>
+      {data && <h2 className='mt-3'>{data.hello}</h2>}
+    </div>
+  )
+
+}
+
+// This was changed 
 function App() {
   const [count, setCount] = useState(0)
 
